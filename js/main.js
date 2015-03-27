822acd62463bd90d825482f05322d0eb99dc09f8
Jamie <james.cranston91@gmail.com>
2015-03-28 01:02:14 +1100

    Content started

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
    $(".services-button").dropdown();
     $(".button-collapse").sideNav();
+    $('.bodyContainer div').hover(function(){
+           $('.businessCard').removeClass('z-depth-3').addClass('z-depth-5');}, function(){
+                $('.businessCard').removeClass('z-depth-5').addClass('z-depth-3');
+            });
     
 });
