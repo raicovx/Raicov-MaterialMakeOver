53b91b51b1b5da9d9446c18994fc8357ba409144
Jamie <james.cranston91@gmail.com>
2015-03-28 23:54:06 +1100

    Transitioned to SASS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
    $(".services-button").dropdown();
     $(".button-collapse").sideNav();
-    $('.bodyContainer div').hover(function(){
-           $('.businessCard').removeClass('z-depth-3').addClass('z-depth-5');}, function(){
-                $('.businessCard').removeClass('z-depth-5').addClass('z-depth-3');
+    $('.container div').hover(function(){
+           $('.businessCard').removeClass('z-depth-2').addClass('z-depth-4');}, function(){
+                $('.businessCard').removeClass('z-depth-4').addClass('z-depth-2');
             });
     
 });
